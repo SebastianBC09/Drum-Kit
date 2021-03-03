8070e7bf62a94b8e51db3ace5a0c1ebe8a3243c6
Sebastian Ballen <sebastian.ballenc@gmail.com>
2021-03-03 18:29:07 -0500

    After sounds have been added to a corresponding button. Now more event listeners were added & functions. An event listener was created to know when a key is pressed in order to trigger a function with the sound of it. A function was created to detect those key events. Finally, a function was created in order to animate the button that is being pressed.

--- index.js
+++ index.js
@@ -1,50 +1,67 @@
 var buttons = document.querySelectorAll(".drum").length
-/* var audio = new Audio('sounds/tom-1.mp3') */
 
-for (var i = 0; i < buttons ; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+for (var i = 0; i < buttons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML
-
-    switch (buttonInnerHTML) {
-      case "w":
-        var crash = new Audio('sounds/crash.mp3')
-        crash.play()
-        break;
-
-      case "a":
-        var kick = new Audio('sounds/kick-bass.mp3')
-        kick.play()
-        break;
-
-      case "s":
-        var snare = new Audio('sounds/snare.mp3')
-        snare.play()
-        break;
-
-      case "d":
-        var tom1 = new Audio('sounds/tom-1.mp3')
-        tom1.play()
-        break;
-
-      case "j":
-        var tom2 = new Audio('sounds/tom-2.mp3')
-        tom2.play()
-        break;
-
-      case "k":
-        var tom3 = new Audio('sounds/tom-3.mp3')
-        tom3.play()
-        break;
-
-      case "l":
-        var tom4 = new Audio('sounds/tom-4.mp3')
-        tom4.play()
-        break;
-
-      default:
-        console.log(buttonInnerHTML)
-        break;
-    }
-
+    checkCharacter(buttonInnerHTML)
+    buttonAnimation()
   })
+}
+
+document.addEventListener("keydown", function (event) {
+  checkCharacter(event.key)
+  buttonAnimation(event.key)
+})
+
+function checkCharacter(key) {
+  switch (key) {
+    case "w":
+      var crash = new Audio('sounds/crash.mp3')
+      crash.play()
+      break;
+
+    case "a":
+      var kick = new Audio('sounds/kick-bass.mp3')
+      kick.play()
+      break;
+
+    case "s":
+      var snare = new Audio('sounds/snare.mp3')
+      snare.play()
+      break;
+
+    case "d":
+      var tom1 = new Audio('sounds/tom-1.mp3')
+      tom1.play()
+      break;
+
+    case "j":
+      var tom2 = new Audio('sounds/tom-2.mp3')
+      tom2.play()
+      break;
+
+    case "k":
+      var tom3 = new Audio('sounds/tom-3.mp3')
+      tom3.play()
+      break;
+
+    case "l":
+      var tom4 = new Audio('sounds/tom-4.mp3')
+      tom4.play()
+      break;
+
+    default:
+      console.log("Incorrect key pressed")
+      break;
+  }
+}
+
+function  buttonAnimation (currentKey) {
+  var currentButton = document.querySelector("." + currentKey)
+  currentButton.classList.add("pressed")
+
+  setTimeout( function() {
+    currentButton.classList.remove("pressed")
+  }, 100);
+
 }
